3f664b0972aa3cdfe483f15bc25ab154101ec7e8
Georgesui <gdimov445@gmail.com>
2022-04-14 16:47:35 +0300

    final commit

--- script.js
+++ script.js
@@ -5,18 +5,16 @@ const containerBody = document.querySelector('.container')
 
 containerBody.addEventListener('click', (event) => {
 	let newLiInList = document.createElement('li');
-	if (event.target === buttonNeeded) {
+	if (event.target === buttonNeeded && ourInputOnPage.value !== '') {
 		newLiInList.innerHTML = `${ourInputOnPage.value} <button class = "remove">REMOVE</button>`
 		ourInputOnPage.value = '';
 		ourList.appendChild(newLiInList).setAttribute('class', 'process');
 		return;
 	}
-
 	else if (event.target.className === "remove") {
 		event.target.closest('li').remove();
 		return;
 	}
-	
 	else if (event.target.closest('li')) {
 		event.target.closest('li').classList.toggle('done');
 		return;
